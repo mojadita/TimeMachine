098d6ad59142b6b56ff784eec7be231d64cdc9f9
Luis Colorado <lcu@basen.net>
2015-12-02 15:03:37 +0200

    Write javadoc information

--- TimeMachine/src/net/basen/timecontroller/package-info.java
+++ TimeMachine/src/net/basen/timecontroller/package-info.java
@@ -1,4 +1,4 @@
-/**
+/*
  * $Id: $
  *
  * Copyright (C) 2015 BaseN.
@@ -6,7 +6,8 @@
  * All rights reserved.
  */
 /**
- * @author lcu
+ * Package for the development of the Timemanager application widget.
+ * @author Luis Colorado <code>&lt;lcu@basen.net&gt;</code>
  *
  */
 package net.basen.timecontroller;
